a2ff626fb35dec60bd98e901cb7e84fad454f6ba
nw51631 <127346229+nw51631@users.noreply.github.com>
2023-05-04 10:50:00 +0100

    Add files via upload

---
+++ OOPa2ver4/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    public class Card
+    {
+        
+        public int Value { get; set; }
+        public int Suit { get; set; }
+    }
+    public class Suit
+    
+    {
+        public string Name { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
