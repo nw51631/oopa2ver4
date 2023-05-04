4eb0f4c06e7d3336c9d35187e18806b8ac766145
nw51631 <127346229+nw51631@users.noreply.github.com>
2023-05-04 13:25:44 +0100

    Update Program.cs

--- OOPa2ver4/CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ OOPa2ver4/CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -8,9 +8,21 @@ namespace CMP1903M_A01_2223
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void CardSort(int[] array)
         {
-
+            int num = array.Length;
+            for (int var1 = 0; var1 < num - 1; var1++)
+            {
+                for (int var2 = var1 + 1; var2 < num; var2++)
+                {
+                    if (array[var1] > array[var2])
+                    {
+                        int temp = array[var1];
+                        array[var1] = array[var2];
+                        array[var2] = temp;
+                    }
+                }
+            }
         }
     }
 }
